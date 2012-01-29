42125d7b5782f85d27476b0fa01db0a791b9a295
Ryan Brown <ryansb@csh.rit.edu>
2012-01-29 14:34:04 -0500

    Import zen

--- ish/resources/system.py
+++ ish/resources/system.py
@@ -22,8 +22,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from ish.resources import ImpulseObject, ImmutabilityMeta
 from ish import get_username
+from ish.resources import ImpulseObject, ImmutabilityMeta
 
 
 class System(ImpulseObject):
